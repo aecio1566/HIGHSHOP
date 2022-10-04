1eb193ed953d38e77765de40d288a1ee8ec21af6
aecio1566 <105462343+aecio1566@users.noreply.github.com>
2022-10-03 21:39:01 -0300

    Create cadastro1.h

---
+++ cadastro1.h
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro HighShop</title>
+    <link rel="stylesheet" href="./assets/css/cadastro1.css">
+</head>
+
+<body>
+    <section class="container">
+        <nav>
+            <p class="passo">1</p>
+            <h1>Sou um(a)...</h1>
+            <p class="opcao">
+                <a href="./cadastro2.html" class="comprador">Comprador(a)</a>
+            </p>
+            <p class="opcao">
+                <a href="./cadastro3.html" class="vendedor">Vendedor(a)</a>
+            </p>
+        </nav>
+    </section>
+</body>
+
+</html>
