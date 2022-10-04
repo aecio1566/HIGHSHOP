b7ce94078472a9a83add5aa316871634c491f232
aecio1566 <105462343+aecio1566@users.noreply.github.com>
2022-10-03 21:40:31 -0300

    Create cadastro3.h

---
+++ cadastro3.h
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro HighShop</title>
+    <link rel="stylesheet" href="./assets/css/cadastro2.css">
+</head>
+
+<body>
+    <section class="container">
+        <p class="passo">2</p>
+        <h1>Agora, nos diga os seguintes dados:</h1>
+        <div class="cadastro">
+            <form action="/" class="form" method="post">
+                <label for="nome">Nome da Loja</label>
+                <input type="text" />
+                <label for="email">Email</label>
+                <input type="text" />
+                <label for="senha">Senha</label>
+                <input type="text" />
+                <label for="senha">Insira novamente</label>
+                <input type="text" />
+                <button type="submit"><a href="./index.html">Cadastrar</a></button>
+            </form>
+        </div>
+    </section>
+</body>
