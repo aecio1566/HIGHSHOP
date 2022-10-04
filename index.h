4dde52e66896690b7c7317d80b248f6cb0fe977a
aecio1566 <105462343+aecio1566@users.noreply.github.com>
2022-10-03 21:37:45 -0300

    Create index.h

---
+++ index.h
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>HighShop</title>
+    <link rel="stylesheet" href="./assets/css/index.css" />
+</head>
+
+<body>
+    <section class="container">
+        <p class="titulo">
+            Garimpe roupas de marca na <strong>HighShop.</strong>
+        </p>
+        <p class="subtitulo">Faça o login e coloque o seu thrifting em dia:</p>
+        <div class="login">
+            <form action="/" class="form" method="post">
+                <label for="login"> Login</label>
+                <input type="text" />
+                <label for="senha">Senha</label>
+                <input type="text" />
+                <button type="submit">Entrar</button>
+            </form>
+        </div>
+        <p class="cadastro">
+            Não possui conta? <a href="./cadastro1.html" class="a">Cadastre-se</a>
+        </p>
+    </section>
+</body>
+
+</html>
